bb5cd576492f22155f3b4c82940ff79be0a185f2
Mamelski <jakub.mamelski@outlook.com>
2019-12-07 11:10:20 +0100

    Add empty project

---
+++ photo-printing-dispatcher/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace photo_printing_dispatcher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
